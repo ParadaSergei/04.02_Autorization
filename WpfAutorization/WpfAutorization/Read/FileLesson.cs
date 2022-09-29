13470d8bf77d3fc803e60ec9b9a5854ad2a7b701
Парада Сергей Сергеевич <106469820+ParadaSergei@users.noreply.github.com>
2022-09-29 20:51:02 +0300

    Add files via upload

---
+++ WpfAutorization/WpfAutorization/Read/FileLesson.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAutorization
+{
+
+   public class FileLesson
+    {
+        public ObservableCollection<User> FileLess()
+        {
+            {
+                var lessonlist = new ObservableCollection<User>();
+
+                using (StreamReader reader = new StreamReader(@"C:\Задачки\WpfAutorzation\WpfAutorization\WpfAutorization\Document\Lesson.txt"))
+                {
+                    foreach (var item in reader.ReadToEnd().Split('\n'))
+                    {
+                        var arrayString = item.Split('?');
+                        if (arrayString[0] != "ID")
+                        {
+                            var user = new User()
+                            {
+                                ID = arrayString[0],
+                                Object = arrayString[1],
+                            };
+
+                            lessonlist.Add(user);
+                        }
+                    }
+                }
+                return lessonlist;
+            }
+            
+        }
+
+    }
+}
