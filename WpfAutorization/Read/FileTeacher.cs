8c17f9c37289ca7630255447243688b1458c9b82
Парада Сергей Сергеевич <106469820+ParadaSergei@users.noreply.github.com>
2022-09-29 21:25:51 +0300

    Add files via upload

---
+++ WpfAutorization/Read/FileTeacher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAutorization
+{
+    class FileTeacher
+    {
+        public ObservableCollection<User> FileTeach()
+        {
+
+                var teacherlist = new ObservableCollection<User>();
+
+                using (StreamReader read = new StreamReader(@"..\..\Document\Teachers.txt"))
+                {
+                    foreach (var item in read.ReadToEnd().Split('\n'))
+                    {
+                        var arrayString = item.Split(',');
+                        if (arrayString[0] != "ID")
+                        {
+                            var user = new User()
+                            {
+                                ID = arrayString[0],
+                                Teachers = arrayString[1],
+                            };
+                              teacherlist.Add(user);
+                        }
+                    }
+                }
+                return teacherlist;
+            }
+    }
+}
